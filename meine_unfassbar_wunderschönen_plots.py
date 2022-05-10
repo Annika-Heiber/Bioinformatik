f64f706b8da41d48636a492c15874d7d911ccfac
Annika Heiber <annika.heiber@th-bingen.de>
2022-05-10 09:15:35 +0200

    add data in meine unfassbar...

--- meine_unfassbar_wunderschönen_plots.py
+++ meine_unfassbar_wunderschönen_plots.py
@@ -7,3 +7,12 @@ df = pd.DataFrame({"Day 1": [7,1,5,6,3,10,5,8],
 
 sns.lineplot(data = df)
 plt.savefig('filename.png')
+
+
+
+#6. Plotten Sie die Funktion f (x) = x2 f ̈ur Werte von x von -10 bis +10. (Datei
+#plot x power 2.png)
+#7. Plotten Sie die Funktion g(x) = x3 f ̈ur Werte von x von -10 bis +10. (Datei
+#plot x power 3.png)
+#8. Plotten Sie beide Funktionen f in blau und g in rot in denselben Graphen. (Datei
+#plot x power 2 and 3.png)
